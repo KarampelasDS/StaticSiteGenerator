1cd82a88ba1c9ae12516cf90639b82a86a37685b
KarampelasDS <24dimitris@gmail.com>
2026-05-21 19:46:03 +0300

    Project completion

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -81,10 +81,10 @@ def split_nodes_link(old_nodes):
     return new_nodes
 
 def text_to_textnodes(text):
-    result = []
-    result = split_nodes_image([TextNode(text, TextType.TEXT)])
-    result = split_nodes_link(result)
+    result = [TextNode(text, TextType.TEXT)]
     result = split_nodes_delimiter(result, "**", TextType.BOLD)
     result = split_nodes_delimiter(result, "_", TextType.ITALIC)
     result = split_nodes_delimiter(result, "`", TextType.CODE)
+    result = split_nodes_image(result)
+    result = split_nodes_link(result)
     return result
